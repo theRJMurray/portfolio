2ee9b232ce8e7d25b4f43d1b1ccb6ade1b298049
RJ Murray <therjmurray@gmail.com>
2024-11-01 16:37:49 -0400

    basic setup

---
+++ app/components/Header.js
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return <div style={{width: '100%', height: 400, borderBottom: '1px solid black'}}>
+        <div>Sales</div>
+        <div>Marketing</div>
+        <div>Coding</div>
+    </div>
+}
+
+export default Header
---
+++ app/components/Navigation.js
@@ -0,0 +1,11 @@
+
+
+const Navigation = () => {
+    return <div style={{height: 86, background: '#FFF', borderBottom: '1px solid black'}}>
+        <div>
+            Richard Murray
+        </div>
+    </div>
+}
+
+export default Navigation;
